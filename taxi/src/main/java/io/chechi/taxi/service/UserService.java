612157c9ece7a783485c00c9f1cf74bea81b080f
xChechi <stefannasev81@gmail.com>
2024-08-15 14:56:09 +0100

    User Service interface added

---
+++ taxi/src/main/java/io/chechi/taxi/service/UserService.java
@@ -0,0 +1,25 @@
+package io.chechi.taxi.service;
+
+import io.chechi.taxi.dto.UserRequest;
+import io.chechi.taxi.dto.UserResponse;
+import io.chechi.taxi.dto.UserUpdatePasswordRequest;
+import io.chechi.taxi.dto.UserUpdateRequest;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserResponse> findAllUsers ();
+
+    UserResponse findUserById (Integer id);
+
+    boolean existByEmail(String email);
+
+    void deleteUserById (Integer id);
+
+    UserResponse createUser (UserRequest request);
+
+    UserResponse updateUser (Integer id, UserUpdateRequest request);
+
+    UserResponse updateUserPassword (Integer id, UserUpdatePasswordRequest request);
+}
