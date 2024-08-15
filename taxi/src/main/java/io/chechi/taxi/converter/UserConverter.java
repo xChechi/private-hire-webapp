70e20d28b586feeaa5dcde1344d6a6ea7c6baf05
xChechi <stefannasev81@gmail.com>
2024-08-15 14:20:50 +0100

    User Converter added

---
+++ taxi/src/main/java/io/chechi/taxi/converter/UserConverter.java
@@ -0,0 +1,40 @@
+package io.chechi.taxi.converter;
+
+import io.chechi.taxi.dto.UserRequest;
+import io.chechi.taxi.dto.UserResponse;
+import io.chechi.taxi.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@AllArgsConstructor
+public class UserConverter {
+
+    //private final PasswordEncoder passwordEncoder;
+
+    public User createUser (UserRequest request) {
+
+        return User.builder()
+                .username(request.getUsername())
+                .password(request.getPassword()) //<-- Don't forget to encode password in DB
+                .email(request.getEmail())
+                .role(request.getRole())
+                .phoneNumber(request.getPhoneNumber())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public UserResponse toUserResponse (User user) {
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .createdAt(user.getCreatedAt())
+                .role(user.getRole())
+                .build();
+    }
+
+
+}
